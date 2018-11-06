296be7421cb67cec8081c71e2e627ac0c3eac5d9
Jan Hornych <hornych.h@gmail.com>
2018-11-06 08:38:31 +0100

    refactoring

--- src/test/java/tesena/automation/training/tests/example_5/PageObjectsTest.java
+++ src/test/java/tesena/automation/training/tests/example_5/PageObjectsTest.java
@@ -1,9 +1,6 @@
 package tesena.automation.training.tests.example_5;
 
 import org.testng.annotations.Test;
-import tesena.automation.training.data.User;
-import tesena.automation.training.data.Users;
-import tesena.automation.training.factory.DataFactory;
 import tesena.automation.training.objects.DefaultPage;
 import tesena.automation.training.test.RedmineTest;
 
