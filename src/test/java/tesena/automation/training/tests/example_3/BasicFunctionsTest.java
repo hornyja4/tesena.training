510dd2a95d5c17ede359ca12968c27bd0bdd5517
Johny <hornych.@gmail.com>
2019-06-06 08:42:51 +0200

    - preparing for training

---
+++ src/test/java/tesena/automation/training/tests/example_3/BasicFunctionsTest.java
@@ -0,0 +1,13 @@
+package tesena.automation.training.tests.example_3;
+
+import org.testng.annotations.Test;
+import tesena.automation.training.test.SimpleTest;
+
+public class BasicFunctionsTest extends SimpleTest {
+
+    @Test
+    public void basicFunctions() {
+
+    }
+
+}
--- src/test/java/tesena/automation/training/tests/example_5/ListenerTest.java
+++ src/test/java/tesena/automation/training/tests/example_5/ListenerTest.java
@@ -1,8 +1,9 @@
 package tesena.automation.training.tests.example_5;
 
 import org.testng.annotations.Test;
+import tesena.automation.training.test.SimpleTest;
 
-public class ListenerTest {
+public class ListenerTest extends SimpleTest {
 
     @Test
     public void testWithListener() {
